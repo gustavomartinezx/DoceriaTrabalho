816485a9f87cdac3b5ee86d09d514a40b8fd7285
gustavomartinezx <gustavopalmr@gmail.com>
2024-05-14 16:13:35 -0300

    Modificacoes valorTotal

--- src/Pedido.java
+++ src/Pedido.java
@@ -17,29 +17,35 @@ public class Pedido {
                    [3]doce de uva
                    [4]brigadeiro de leito ninho
                     """);
-
         int opcao = entrada.nextInt();
+
+        System.out.println("Quantas unidades você irá querer?");
+        float quantidadeDoces = entrada.nextFloat();
+
         switch (opcao) {
             case 1:
                 this.item = "brigadeiro";
+                valorTotal = (float) (1.0 * quantidadeDoces);
                 break;
             case 2:
                 this.item = "beijinho";
+                valorTotal = (float) (1.5 * quantidadeDoces);
                 break;
             case 3:
                 this.item = "doce de uva";
+                valorTotal = (float) (3.0 * quantidadeDoces);
                 break;
             case 4:
                 this.item = "brigadeiro de leite ninho";
+                valorTotal = (float) (2.0 * quantidadeDoces);
                 break;
             default:
                 System.out.println("Opção inválida.");
                 break;
         }
 
-        System.out.println("Digite o valor total do pedido:");
-        this.valorTotal = entrada.nextFloat();
-        entrada.nextLine();
+        System.out.println("Digite quantas unidades você irá querer:");
+        float unidadeDoce = entrada.nextFloat();
 
         this.cliente = cliente;
         cliente.adicionarPedido(this);
